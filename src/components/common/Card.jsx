585346cb6e8f3d224a1702148b41b34ab08b9ebf
hrithikscube <hrithik@scube.me>
2023-09-05 15:51:42 +0530

    clean up

--- src/components/common/Card.jsx
+++ src/components/common/Card.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Card = ({item}) => {
     return (
         <div className="mr-4 hover:scale-[0.92] transition-all cursor-pointer">
-            <img src={item.image} alt="food_category_card" className="h-[180px] object-cover lg:w-[280px] w-full rounded-[12px]" />
+            <img src={item.image} alt="food_category_card" className="h-[160px] object-cover lg:w-[280px] w-full rounded-[12px]" />
             <div className="flex flex-col mt-2">
                 <p className="text-lg font-poppins font-medium  text-footerDark">{item.name}</p>
                 <div className="flex items-center gap-2">
