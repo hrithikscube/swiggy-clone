74b73291ef1d3cc24bfe3170e27e4c3d85cd897f
hrithikscube <hrithik@scube.me>
2023-09-11 16:39:25 +0530

    updated pages

--- src/components/modules/Login.jsx
+++ src/components/modules/Login.jsx
@@ -29,7 +29,7 @@ const Login = () => {
         <div className='h-screen flex flex-row items-start'>
 
             <div className='lg:w-7/12 lg:block hidden'>
-                <img src="https://picsum.photos/1440/800?grayscale" alt="" className='h-screen w-full object-cover' />
+                <img src="https://picsum.photos/1440/800?grayscale" alt="" className='lg:block hidden h-screen w-full object-cover' />
             </div>
 
             <div className='lg:w-5/12 bg-white lg:p-10 p-5 flex flex-col w-full gap-4 justify-center items-center'>
