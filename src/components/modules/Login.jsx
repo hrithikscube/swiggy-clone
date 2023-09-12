6cf4fa15466f6d4ce77d70c4e1196c56b40298ab
hrithikscube <hrithik@scube.me>
2023-09-12 08:30:18 +0530

    updated login image url

--- src/components/modules/Login.jsx
+++ src/components/modules/Login.jsx
@@ -30,7 +30,7 @@ const Login = () => {
         <div className='h-screen flex flex-row items-center'>
 
             <div className='lg:w-7/12 lg:block hidden'>
-                <img src="https://picsum.photos/1440/800?grayscale" alt="" className='lg:block hidden h-screen w-full object-cover' />
+                <img src="https://source.unsplash.com/random/1440*800?food" alt="" className='lg:block hidden h-screen w-full object-cover' />
             </div>
 
             <div className='lg:w-5/12 bg-white lg:p-10 p-5 flex flex-col w-full gap-4 justify-center items-center'>
